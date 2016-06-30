cfe168050e8151249011a048235f313bbe8972a7
Emmet Murphy <emmet@emurphy.net>
2016-06-30 15:07:03 -0700

    initial scripts and sample CSV, png

---
+++ speedtest.sh
@@ -0,0 +1,10 @@
+# compliments of http://osxdaily.com/2013/07/31/speed-test-command-line/
+
+for i in {1..10}
+do
+  curl -o /dev/null -s -w ',%{speed_download}' http://speedtest.wdc01.softlayer.com/downloads/test10.zip | ts >> speedtest.csv
+  # add \n at end of line
+  sed -i -e '$a\' speedtest.csv
+done
+
+
