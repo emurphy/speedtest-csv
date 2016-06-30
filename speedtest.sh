d04187ea03a11afc724c05082614f7941d92ba56
Emmet Murphy <emmet@emurphy.net>
2016-06-30 15:16:16 -0700

    infinite loop with 1-minute sleep

--- speedtest.sh
+++ speedtest.sh
@@ -1,10 +1,11 @@
 # compliments of http://osxdaily.com/2013/07/31/speed-test-command-line/
 
-for i in {1..10}
+while true
 do
   curl -o /dev/null -s -w ',%{speed_download}' http://speedtest.wdc01.softlayer.com/downloads/test10.zip | ts >> speedtest.csv
   # add \n at end of line
   sed -i -e '$a\' speedtest.csv
+  sleep 60
 done
 
 
