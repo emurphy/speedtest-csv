7028e092ef6765eda269d6fe8d70ecd534b8e9a3
emurphy <emmet@dealbase.com>
2017-09-07 19:16:35 -0700

    analyze a single server

--- speedtest.R
+++ speedtest.R
@@ -1,17 +1,12 @@
-laptop_csv <- 'speedtest-laptop-wifi.csv'
-desktop_csv <- 'speedtest-desktop-cable.csv'
-laptop_df <- read.csv(laptop_csv, header=FALSE)
-desktop_df <- read.csv(desktop_csv, header=FALSE)
-names(laptop_df) = c("datetime_str", "bytes_per_second")
-names(desktop_df) = c("datetime_str", "bytes_per_second")
+csv <- 'speedtest-cable-to-2017-09-05.csv'
+df <- read.csv(csv, header=FALSE)
+names(df) = c("datetime_str", "bytes_per_second")
 
 library(dplyr)
-speed_laptop <- mutate(laptop_df, mbits_per_second = bytes_per_second * 8 / 1000000, system="laptop wifi")
-speed_laptop <- transform(speed_laptop, datetime=strptime(datetime_str, "%b %d %H:%M:%S "))
-speed_desktop <- mutate(desktop_df, mbits_per_second = bytes_per_second * 8 / 1000000, system="desktop cable")
-speed_desktop <- transform(speed_desktop, datetime=strptime(datetime_str, "%b %d %H:%M:%S "))
+speed <- mutate(df, mbits_per_second = bytes_per_second * 8 / 1000000, system="desktop cable")
+speed <- transform(speed, datetime=strptime(datetime_str, "%b %d %H:%M:%S "))
 
-speed <- rbind(speed_laptop, speed_desktop)
+#speed <- rbind(speed_laptop, speed_desktop)
 
 library(ggplot2)
 png("speedtest.png", width=1000, height=700)
@@ -19,10 +14,10 @@ g <- ggplot(speed, aes(x=datetime, y=mbits_per_second, group=system, color=syste
 g + geom_line()
 dev.off()
 
-png("speedtest-side-by-side.png", width=1000, height=700)
-grid <- ggplot(speed, aes(x=datetime, y=mbits_per_second))
-grid + facet_grid(. ~ system) + geom_line()
-dev.off()
+#png("speedtest-side-by-side.png", width=1000, height=700)
+#grid <- ggplot(speed, aes(x=datetime, y=mbits_per_second))
+#grid + facet_grid(. ~ system) + geom_line()
+#dev.off()
 
 png("speedtest-boxplot.png", width=1000, height=700)
 box <- ggplot(speed, aes(factor(system), mbits_per_second))
--- speedtest.sh
+++ speedtest.sh
@@ -5,7 +5,7 @@ do
   curl -o /dev/null -s -w ',%{speed_download}' http://speedtest.wdc01.softlayer.com/downloads/test10.zip | ts >> speedtest.csv
   # add \n at end of line
   sed -i -e '$a\' speedtest.csv
-  sleep 300
+  sleep 600
 done
 
 
